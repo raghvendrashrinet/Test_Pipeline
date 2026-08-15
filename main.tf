d7c7299621e4ba1fd1585754042dbdeae7e1c544
DESKTOP-HUO8ATD\rag <raghvendra.ses@gmail.com>
2026-08-15 11:42:14 +0530

    run workflow

--- main.tf
+++ main.tf
@@ -12,6 +12,6 @@ resource "azurerm_virtual_network" "vnet-1" {
   resource_group_name = azurerm_resource_group.rg.name
   name                = "vnet1"
   location            = var.location
-   address_space       = ["10.0.0.0/16"]
+  address_space       = ["10.0.0.0/16"]
 }
 
